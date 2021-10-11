2475e73e3e01fe6b8c519fe9c5ecd002ad04a5d1
pfcoder <pfcoder97@gmail.com>
2021-10-11 23:22:03 +0800

    update

---
+++ src/views/dashboard/admin/components/BarChart.vue
@@ -0,0 +1,149 @@
+<template>
+  <div :class="className" :style="{height:height,width:width}" />
+</template>
+
+<script>
+import echarts from 'echarts'
+require('echarts/theme/macarons') // echarts theme
+import resize from './mixins/resize'
+
+const animationDuration = 6000
+
+export default {
+  mixins: [resize],
+  props: {
+    className: {
+      type: String,
+      default: 'chart'
+    },
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '300px'
+    },
+    chartData: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      chart: null
+    }
+  },
+  watch: {
+    chartData: {
+      deep: true,
+      handler(val) {
+        this.setOptions(val)
+      }
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.initChart()
+    })
+  },
+  beforeDestroy() {
+    if (!this.chart) {
+      return
+    }
+    this.chart.dispose()
+    this.chart = null
+  },
+  methods: {
+    initChart() {
+      this.chart = echarts.init(this.$el, 'macarons')
+      this.setOptions(this.chartData)
+    },
+
+    setOptions(data) {
+      const names = []
+      const oilTemp1 = []
+      const oilTemp2 = []
+      const oilTemp3 = []
+      const oilTemp4 = []
+      const waterInTemp = []
+      const waterOutTemp = []
+      for (let i = 0; i < data.length; i++) {
+        names.push(data[i][0])
+        oilTemp1.push(data[i][1])
+        oilTemp2.push(data[i][2])
+        oilTemp3.push(data[i][3])
+        oilTemp4.push(data[i][4])
+        waterInTemp.push(data[i][5])
+        waterOutTemp.push(data[i][6])
+      }
+
+      this.chart.setOption({
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: { // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        grid: {
+          top: 10,
+          left: '2%',
+          right: '2%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [{
+          type: 'category',
+          data: names,
+          axisTick: {
+            alignWithLabel: true
+          }
+        }],
+        yAxis: [{
+          type: 'value',
+          axisTick: {
+            show: false
+          }
+        }],
+        series: [{
+          name: '油温1',
+          type: 'bar',
+          barWidth: '10%',
+          data: oilTemp1,
+          animationDuration
+        }, {
+          name: '油温2',
+          type: 'bar',
+          barWidth: '10%',
+          data: oilTemp2,
+          animationDuration
+        }, {
+          name: '油温3',
+          type: 'bar',
+          barWidth: '10%',
+          data: oilTemp3,
+          animationDuration
+        }, {
+          name: '油温4',
+          type: 'bar',
+          barWidth: '10%',
+          data: oilTemp4,
+          animationDuration
+        }, {
+          name: '入水温',
+          type: 'bar',
+          barWidth: '10%',
+          data: waterInTemp,
+          animationDuration
+        }, {
+          name: '出水温',
+          type: 'bar',
+          barWidth: '10%',
+          data: waterOutTemp,
+          animationDuration
+        }]
+      })
+    }
+  }
+}
+</script>
---
+++ src/views/dashboard/admin/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="dashboard-editor-container">
+
+    <el-row :gutter="32">
+      <el-col :xs="24" :sm="24" :lg="24">
+        <div class="chart-wrapper">
+          <bar-chart :chart-data="kongTemperatureData" />
+        </div>
+      </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<script>
+import BarChart from './components/BarChart'
+
+var tempTestData = [['T1', 35, 40, 41, 42, 30, 35], ['T2', 33, 32, 43, 35, 50, 30, 38], ['T3', 33, 32, 43, 35, 50, 30, 38],
+  ['T4', 35, 40, 41, 42, 30, 35], ['T5', 33, 32, 43, 35, 50, 30, 38], ['T6', 33, 32, 43, 35, 50, 30, 38], ['T7', 33, 32, 43, 35, 50, 30, 38]]
+
+export default {
+  name: 'DashboardAdmin',
+  components: {
+    BarChart
+  },
+  data() {
+    return {
+      kongTemperatureData: tempTestData
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard-editor-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+  position: relative;
+
+  .chart-wrapper {
+    background: #fff;
+    padding: 16px 16px 0;
+    margin-bottom: 32px;
+  }
+}
+
+@media (max-width:1024px) {
+  .chart-wrapper {
+    padding: 8px;
+  }
+}
+</style>
